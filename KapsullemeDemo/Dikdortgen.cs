25836dcffdaf422a40d0c28168972edaec40fc94
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:30:03 +0300

    first

---
+++ KapsullemeDemo/Dikdortgen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeDemo
+{
+    public class Dikdortgen
+    {
+        private int a;
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    a = value;
+                }
+            }
+        }
+
+        public int B { get; set; }
+
+        public int Alan
+        {
+            get
+            {
+                if (A < 0 || B <0)
+                {
+                    return 0;
+                }
+                return A * B;
+            }
+        }
+
+    }
+}
---
+++ KapsullemeDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dikdortgen = new Dikdortgen();
+            dikdortgen.A = -50;
+            dikdortgen.B = 125;
+
+            Console.WriteLine(dikdortgen.Alan);
+
+            Console.ReadKey();
+
+        }
+    }
+}
